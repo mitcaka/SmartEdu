87db9e3d39faf3927e01c96ec7425bfb02c13dfb
Nguyen Mai <67093594+mitcaka@users.noreply.github.com>
2024-12-20 12:12:25 +0700

    Header, Sidebar, Hero

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["res.cloudinary.com", "randomuser.me"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "edmy-react.hibootstrap.com",
+        port: "",
+        pathname: "/images/banner/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
